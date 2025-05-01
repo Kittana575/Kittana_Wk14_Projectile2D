d94b83ee5464c532f19fc029d5123cd98eab4621
Kittana575 <kittana.tean@bumail.net>
2025-05-01 13:51:12 +0700

    Final

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -18,11 +18,28 @@ public class NewMonoBehaviourScript : MonoBehaviour
             if (hit.collider != null)
             {
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                
                 Debug.Log("hit" + hit.collider.name);
+                
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point,1f);
+                
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab,shootPoint.position,Quaternion.identity);
+                
+                shootBullet.linearVelocity = projectileVelocity;
             }
         }
-        
-        
-        
+
+        Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+        {
+            Vector2 distance = target - origin;
+            
+            float velocityX = distance.x / time;
+            float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+            
+            Vector2 projectileVelocity = new Vector2(velocityX,velocityY);
+            
+            return projectileVelocity;
+        }
+       
     }
 }
